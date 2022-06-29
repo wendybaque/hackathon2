07365742805dda172eb65cc1fbdc88703c74487e
Baasile <mathieu.basile@gmail.com>
2022-06-29 21:10:22 +0200

    create MyProject + json fakedata

--- frontend/src/components/MyProjects.jsx
+++ frontend/src/components/MyProjects.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import json from "../data/fakedata";
 
 function MyProjects() {
-  return <div>Hello MyProjects</div>;
+  return json.map((elements) => {
+    return (
+      <div className="flex flex-row">
+        <div>{elements.nom}</div>
+        <div>{elements.description}</div>
+        <div>{elements.tech}</div>
+        <div>{elements.agence}</div>
+        <div>{elements.status}</div>
+      </div>
+    );
+  });
 }
 
 export default MyProjects;
---
+++ frontend/src/data/fakedata.jsx
@@ -0,0 +1,52 @@
+const json = [
+  {
+    nom: "Projet WildCodeSchool",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "Java",
+    agence: "Tours",
+    status: "Finished",
+  },
+  {
+    nom: "Projet de malade",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "hmtl",
+    agence: "Paris",
+    status: "Testing",
+  },
+  {
+    nom: "Projet incroyable",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "Python",
+    agence: "Toulouse",
+    status: "Finished",
+  },
+  {
+    nom: "Projet magnifique",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "Java",
+    agence: "Lyon",
+    status: "FiDeploying",
+  },
+  {
+    nom: "Projet pas ouf",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "css",
+    agence: "Lille",
+    status: "Begining",
+  },
+  {
+    nom: "Projet qui passe",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
+    tech: "Php",
+    agence: "Dunkerque",
+    status: "Early stage",
+  },
+];
+
+export default json;
