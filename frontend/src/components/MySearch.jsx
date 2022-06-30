18321c8db3ea33bf704cca33bae8bed59b8fdead
Mariakarara <claind15@yahoo.fr>
2022-06-30 14:56:57 +0200

    end of MySearch component

--- frontend/src/components/MySearch.jsx
+++ frontend/src/components/MySearch.jsx
@@ -1,39 +1,50 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-nested-ternary */
-
 import React, { useState } from "react";
-import json from "../data/fakeDataMySearch";
+
+// eslint-disable-next-line import/no-named-as-default-member
+import searchData from "../data/fakeDataMySearch";
 import "../App.css";
 
 // eslint-disable-next-line prettier/prettier
 
 function MySearch() {
-  const [list, setList] = useState(json);
+  const [list, setList] = useState(searchData);
   const addToMyFavorite = (id) => {
     setList(list.filter((elements) => elements.id !== id));
   };
   return (
-    <div className="mx-4">
+    <div className="my-10 ">
       <header>
-        <h1 className="text-center"> List of on going project</h1>
+        <h1 className="text-center mx-10 text-3xl text-orange-400 font-bold">
+          List of on going projects
+        </h1>
       </header>
-      <div className="flex flex-row border-2 border-black h-16">
-        <div className="w-1/5 flex items-center ">PROJECTS</div>
-        <div className="w-2/5 flex items-center mr-10 ">DESCRIPTION</div>
-        <div className="w-1/6 flex items-center  ml-10 ">TECH</div>
-        <div className="w-1/6 flex items-center ml-10 ">AGENCY</div>
-        <div className="w-1/6 flex items-center">STATUS</div>
+      <div className="flex flex-row border-2 border-black h-16 my-10">
+        <div className="w-1/5 flex items-center ml-8">PROJECTS</div>
+        <div className="w-2/5 flex items-center ml-8 ">DESCRIPTION</div>
+        <div className="w-1/6 flex items-center ml-8 ">TECH</div>
+        <div className="w-1/6 flex items-center ml-8 ">SKILLS</div>
+        <div className="w-1/6 flex items-center ml-8 ">COORDINATOR</div>
+        <div className="w-1/6 flex items-center ml-8 ">CLIENT</div>
+        <div className="w-1/6 flex items-center ml-8 ">AGENCY</div>
+        <div className="w-1/6 flex items-center ml-8 ">DUE DATE</div>
+        <div className="w-1/6 flex items-center ml-8">STATUS</div>
         <div className="w-20 flex items-center mx-4 ">ADD TO MY FAVORITE</div>
       </div>
       {list.map((elements) => {
         return (
-          <div className="flex flex-row border-2  border-wwhite-200 py-4">
+          <div className="flex flex-row border-2 border-white-200 py-4">
             <div className="w-1/5">{elements.project_title}</div>
-            <div className="w-2/5 mr-10">{elements.description}</div>
-            <div className="w-1/6 ml-10">{elements.tech}</div>
-            <div className="w-1/6 ml-10">{elements.agency}</div>
-            <div className="w-1/6 flex items-center">
+            <div className="w-2/5 mr-8">{elements.project_description}</div>
+            <div className="w-1/6 ml-8">{elements.technologies_used}</div>
+            <div className="w-1/6 ml-8">{elements.required_skills}</div>
+            <div className="w-1/6 ml-8">{elements.project_coordinator}</div>
+            <div className="w-1/6 ml-8">{elements.client}</div>
+            <div className="w-1/6 ml-8">{elements.agency}</div>
+            <div className="w-1/6 ml-8">{elements.project_end_date}</div>
+            <div className="w-1/6 ">
               {elements.status === "pending" ? (
                 <div className="flex justify-center w-2/3 p-4 bg-red-400  rounded-full">
                   Pending
--- frontend/src/data/fakeDataMySearch.js
+++ frontend/src/data/fakeDataMySearch.js
@@ -1,58 +1,82 @@
-const json = [
+const searchData = [
   {
     id: "1",
     project_title: "Projet WildCodeSchool",
-    description:
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "Java",
+    technologies_used: "Java",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Airbus",
     agency: "Tours",
+    project_end_date: "2015-04-01",
     status: "pending",
   },
   {
     id: "2",
     project_title: "Projet de malade",
-    description:
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "hmtl",
+    technologies_used: "hmtl",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Maison du monde",
     agency: "Paris",
+    project_end_date: "2015-04-01",
     status: "in progress",
   },
   {
     id: "3",
-    project_name: "Projet incroyable",
-    description:
+    project_title: "Projet Airbus",
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "Python",
+    technologies_used: "Python",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Carrfour",
     agency: "Toulouse",
+    project_end_date: "2015-04-01",
     status: "pending",
   },
   {
     id: "4",
-    project_title: "Projet magnifique",
-    description:
+    project_title: "Projet Emmaüs",
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "Java",
+    technologies_used: "Java",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Airbus",
     agency: "Lyon",
+    project_end_date: "2015-04-01",
     status: "Finished",
   },
   {
     id: "5",
-    project_title: "Projet pas ouf",
-    description:
+    project_title: "Projet to secret",
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "css",
+    technologies_used: "css",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Airbus",
     agency: "Lille",
+    project_end_date: "2015-04-01",
     status: "in progress",
   },
   {
     id: "6",
     project_title: "Projet qui passe",
-    description:
+    project_description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam debitis ullam odio quaerat consequuntur amet voluptate nisi et, nobis, perspiciatis non! Obcaecati perspiciatis laudantium nulla quis numquam ullam harum tenetur.",
-    tech: "Php",
+    technologies_used: "Php",
+    required_skills: "Agility",
+    project_coordinator: "John.Doe@gmail.com",
+    client: "Airbus",
     agency: "Dunkerque",
+    project_end_date: "2015-04-01",
     status: "pending",
   },
 ];
 
-export default json;
+export default searchData;
